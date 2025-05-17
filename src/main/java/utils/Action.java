9dc77c667108ec4e082f41028a0a40eb045d7477
Jeffrey0522 <jeffreygranados11@gmail.com>
2025-05-17 14:00:57 -0600

    Se implementaron las clases Room y Action

---
+++ src/main/java/utils/Action.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class Action {
+
+    // -> Acciones del Hotel
+    public static final String HOTEL_REGISTER = "hotelRegister";
+    public static final String HOTEL_LIST = "hotelList";
+    public static final String HOTEL_SEARCH = "hotelRequest";
+    public static final String HOTEL_UPDATE = "hotelUpdate";
+    public static final String HOTEL_DELETE = "hotelDelete";
+
+    public static final String HOTEL_REGISTERED = "hotelRegistered";
+    public static final String HOTEL_REQUESTED = "hotelRequested";
+    public static final String HOTEL_UPDATED = "hotelUpdated";
+    public static final String HOTEL_DELETED = "hotelDeleted";
+
+    // -> Acciones de las Habitaciones
+    public static final String ROOM_REGISTER = "roomRegister";
+
+}
