0880e92955e7523c707b201fbc74948dbb6725ad
jimeest16 <jimenasalasthames@gmail.com>
2025-05-20 20:04:57 -0600

    Changes

--- src/main/java/utils/Action.java
+++ src/main/java/utils/Action.java
@@ -4,12 +4,15 @@ public class Action {
 
     // -> Acciones del Hotel
     public static final String HOTEL_REGISTER = "hotelRegister";
+    public static final String HOTEL_NOT_REGISTER="hotelNoTRegister";
     public static final String HOTEL_LIST = "hotelList";
     public static final String HOTEL_SEARCH = "hotelRequest";
     public static final String HOTEL_UPDATE = "hotelUpdate";
     public static final String HOTEL_DELETE = "hotelDelete";
 
+
     public static final String HOTEL_REGISTERED = "hotelRegistered";
+    public static final String HOTEL_NOT_REGISTERED="hotelNoRegistered";
     public static final String HOTEL_REQUESTED = "hotelRequested";
     public static final String HOTEL_UPDATED = "hotelUpdated";
     public static final String HOTEL_DELETED = "hotelDeleted";
