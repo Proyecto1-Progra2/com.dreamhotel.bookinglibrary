48e20cf85a311f95caf8bc4a8ea1131c7123424b
Jeffrey0522 <jeffreygranados11@gmail.com>
2025-05-21 19:44:38 -0600

    Se arregló el view

---
+++ src/main/java/table/HotelTableModel.java
@@ -0,0 +1,41 @@
+package table;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class HotelTableModel {
+    private final SimpleStringProperty hotelNumber;
+    private final SimpleStringProperty hotelName;
+    private final SimpleStringProperty hotelAddress;
+
+    public HotelTableModel(String hotelNumber, String hotelName, String hotelAddress) {
+        this.hotelNumber = new SimpleStringProperty(hotelNumber);
+        this.hotelName = new SimpleStringProperty(hotelName);
+        this.hotelAddress = new SimpleStringProperty(hotelAddress);
+    }
+
+    public String getHotelNumber() {
+        return hotelNumber.get();
+    }
+
+    public SimpleStringProperty hotelNumberProperty() {
+        return hotelNumber;
+    }
+
+    public String getHotelName() {
+        return hotelName.get();
+    }
+
+    public SimpleStringProperty hotelNameProperty() {
+        return hotelName;
+    }
+
+    public String getHotelAddress() {
+        return hotelAddress.get();
+    }
+
+    public SimpleStringProperty hotelAddressProperty() {
+        return hotelAddress;
+    }
+}
+
---
+++ src/main/java/table/RoomTableModel.java
@@ -0,0 +1,51 @@
+package table;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class RoomTableModel {
+    private final SimpleStringProperty roomNumber;
+    private final SimpleStringProperty roomStatus;
+    private final SimpleStringProperty roomStyle;
+    private final SimpleDoubleProperty roomPrice;
+
+    public RoomTableModel(String roomNumber, String roomStatus, String roomStyle, double roomPrice) {
+        this.roomNumber = new SimpleStringProperty(roomNumber);
+        this.roomStatus = new SimpleStringProperty(roomStatus);
+        this.roomStyle = new SimpleStringProperty(roomStyle);
+        this.roomPrice = new SimpleDoubleProperty(roomPrice);
+    }
+
+    public String getRoomNumber() {
+        return roomNumber.get();
+    }
+
+    public SimpleStringProperty roomNumberProperty() {
+        return roomNumber;
+    }
+
+    public String getRoomStatus() {
+        return roomStatus.get();
+    }
+
+    public SimpleStringProperty roomStatusProperty() {
+        return roomStatus;
+    }
+
+    public String getRoomStyle() {
+        return roomStyle.get();
+    }
+
+    public SimpleStringProperty roomStyleProperty() {
+        return roomStyle;
+    }
+
+    public double getRoomPrice() {
+        return roomPrice.get();
+    }
+
+    public SimpleDoubleProperty roomPriceProperty() {
+        return roomPrice;
+    }
+}
+
