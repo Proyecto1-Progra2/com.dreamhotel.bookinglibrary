c52e9116cf7b05bb00e3d3599da3a8a549c64738
Jeffrey0522 <jeffreygranados11@gmail.com>
2025-06-05 01:47:16 -0600

    Nueva accion de booking

--- src/main/java/utils/Action.java
+++ src/main/java/utils/Action.java
@@ -52,7 +52,7 @@ public class Action {
     public static final String BOOKING_NUMBER_NO_EXIST = "bookingNoExist";
     public static final String BOOKING_LIST = "bookingList";
     public static final String BOOKING_DELETE = "bookingDelete";
-
+    public static final String HOTEL_ROOMS_BOOKING = "hotelRoomsBooking";
 
     public static final String HOST_REGISTER = "hostRegister";
     public static final String HOST_NOT_REGISTERED = "host_not_registered";
